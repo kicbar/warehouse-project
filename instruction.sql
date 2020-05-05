73dc82285d958136092113a9774b18bc4e78ee06
kicbar <bartlomiejkicior@gmail.com>
2020-05-05 20:10:39 +0200

    add instruction in PL language

---
+++ instruction.sql
@@ -0,0 +1,14 @@
+/*W celu wprowadzenia danych wywołaj procedure add_value, według poniższego przykładu: */
+
+call add_value(X,Y,Wmin,Wmax);
+
+gdzie:
+X - szerokosc
+Y - dlugosc
+Wmin - wysokosc w najnizszym punkcie
+Wmax - wysokosc w najwyzszym punkcie
+
+
+/*W celu pobrania danych wykonaj poniższe zapytanie: */
+
+select * from zestawienie;
